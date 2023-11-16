7bfcfd660d57cbf9bdbf581980025f6b36b7adb4
Tom Greenwood <tom.t.greenwood@gmail.com>
2023-11-16 14:51:19 +0100

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+
+#Set here the home path for the directory of the project
